f5feeec93742fff42f552721c0b9ccada8d80104
Lyu80V <7222707@gmail.com>
2024-06-14 20:07:03 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,12 @@
+grades_1 = 5, 3, 3, 5, 4
+grades_2 = 2, 2, 2, 3
+grades_3 = 4, 5, 5, 2
+grades_4 = 4, 4, 3
+grades_5 = 5, 5, 5, 4, 5
+summa_1 = 5 + 3 + 3 + 5 + 4
+summa_2 = 2 + 2 + 2 + 3
+summa_3 = 4 + 5 + 5 + 2
+summa_4 = 4 + 4 + 3
+summa_5 = 5 + 5 + 5 + 4 + 5
+students = {'Aaron': (summa_1/len(grades_1)), 'Bilbo': (summa_2/len(grades_2)), 'Johnny': (summa_3/len(grades_3)), 'Khendrik': (summa_4/len(grades_4)), 'Steve': (summa_5/len(grades_5))}
+print(students)
