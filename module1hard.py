71ee38ecde37769d6520484d543925b458958b20
Lyu80V <7222707@gmail.com>
2024-07-25 17:02:31 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,12 +1,7 @@
-grades_1 = 5, 3, 3, 5, 4
-grades_2 = 2, 2, 2, 3
-grades_3 = 4, 5, 5, 2
-grades_4 = 4, 4, 3
-grades_5 = 5, 5, 5, 4, 5
-summa_1 = 5 + 3 + 3 + 5 + 4
-summa_2 = 2 + 2 + 2 + 3
-summa_3 = 4 + 5 + 5 + 2
-summa_4 = 4 + 4 + 3
-summa_5 = 5 + 5 + 5 + 4 + 5
-students = {'Aaron': (summa_1/len(grades_1)), 'Bilbo': (summa_2/len(grades_2)), 'Johnny': (summa_3/len(grades_3)), 'Khendrik': (summa_4/len(grades_4)), 'Steve': (summa_5/len(grades_5))}
-print(students)
+students = {'Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve'}
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+average_grades = [sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4]),]
+students = sorted(students)
+dict_ = dict(zip(students, average_grades))
+
+print(dict_)
